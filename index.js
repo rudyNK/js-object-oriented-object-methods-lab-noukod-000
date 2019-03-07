75c3b1708b03f2a1f578b9835a1a598ad5cadcea
Rudy Ravelin <rudy@noukod.com>
2019-03-07 16:47:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+new BoardMember(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+let BoardMember = new BoardMember("Mr. Polished", "New York", "law")
